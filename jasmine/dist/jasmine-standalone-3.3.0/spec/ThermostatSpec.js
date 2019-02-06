d1fa1c9777d0668f6200982c746eaceb5707b033
habakkukyong <habakkukyong@gmail.com>
2019-02-06 16:07:33 +0000

    Add energy usage function

--- jasmine/dist/jasmine-standalone-3.3.0/spec/ThermostatSpec.js
+++ jasmine/dist/jasmine-standalone-3.3.0/spec/ThermostatSpec.js
@@ -4,8 +4,22 @@ describe ("Thermostat", function() {
         thermostat = new Thermostat()
     });
 
-    it('has property temperature defaulting to 20', function() {
-        expect(thermostat.temperature).toBe(20)
+    it('has property temperature default', function() {
+        expect(thermostat.temperature).toBe(thermostat.DEFAULT_TEMP)
+    })
+
+    it('has a minimum temperature of 10 degrees', function() {
+        expect(thermostat.MINIMUM_TEMP).toBe(10)
+    })
+
+    it('has a default power saving mode setting-- ON', function() {
+        expect(thermostat.powerSaver)
+    })
+
+    it('has property maxtemp which is 25 when powersaving and 32 when not', function() {
+        expect(thermostat.maxtemp).toBe(25)
+        thermostat._powerSaveOff()
+        expect(thermostat.maxtemp).toBe(32)
     })
 
     describe ("_up", function() {
@@ -13,5 +27,63 @@ describe ("Thermostat", function() {
             thermostat._up()
             expect(thermostat.temperature).toBe(21)
         })
+        it('has a maximum temp of 25 degrees when power saver is on', function() {
+            for (let counter = 0; counter < 5; counter++){
+                thermostat._up()
+            };
+            expect(function(){thermostat._up();}).toThrowError('cannot increase above 25 degrees when power saver is on')           
+        })
     })
-})
+
+    describe ('_down', function() {
+        it('decrements temperature by one', function() {
+            thermostat._down()
+            expect(thermostat.temperature).toBe(19)
+        })
+
+        it('cannot be reduced below the mimimum temperature', function() {
+            for (let counter = 0; counter < 10; counter++){
+                thermostat._down()
+            };
+            expect(function(){thermostat._down();}).toThrowError('cannot reduce below 10 degrees')
+        })
+    })
+
+    describe ('_powerSaveOff', function() {
+        it("sets powerSaver to 'false'", function() {
+            thermostat._powerSaveOff()
+            expect(thermostat.powerSaver).toBe(false)
+        })
+    })
+
+    describe ('_powerSaveOn', function() {
+        it("sets powerSaver to 'true'", function() {
+            thermostat._powerSaveOff()
+            thermostat._powerSaveOn()
+            expect(thermostat.powerSaver)
+        })
+    })
+
+    describe ('_reset', function() {
+        it("resets the temperature to default temperature", function() {
+            thermostat._down()
+            thermostat._reset()
+            expect(thermostat.temperature).toBe(thermostat.DEFAULT_TEMP)
+        })
+    })
+
+    describe ('_usage', function() {
+        it("evaluates usage as LOW when temp is below 18", function() {
+            thermostat.temperature = 17
+            expect(thermostat._usage()).toBe('low-usage')
+        })
+        it("evaluates usage as MEDIUM when temp is above 18 and below 25", function() {
+            thermostat.temperature = 24
+            expect(thermostat._usage()).toBe('medium-usage')          
+        })
+        it("evaluates usage as HIGH when temp is above 25", function() {
+            thermostat.temperature = 26
+            expect(thermostat._usage()).toBe('high-usage')           
+        })
+    })
+}) 
--- jasmine/dist/jasmine-standalone-3.3.0/src/Thermostat.js
+++ jasmine/dist/jasmine-standalone-3.3.0/src/Thermostat.js
@@ -1,7 +1,49 @@
 function Thermostat () {
-    this.temperature = 20
+    this.DEFAULT_TEMP = 20
+    this.temperature = this.DEFAULT_TEMP
+    this.MINIMUM_TEMP = 10
+    this.powerSaver = true
+    this.maxtemp = 25
 };
 
 Thermostat.prototype._up = function() {
-    this.temperature ++
+    if(this.powerSaver === true) {
+        if(this.temperature >= 25) {
+            throw new Error('cannot increase above 25 degrees when power saver is on')
+        } else {
+            this.temperature ++
+        }
+    }
+}
+
+Thermostat.prototype._down = function() {
+    if(this.temperature <= this.MINIMUM_TEMP) {
+        throw new Error('cannot reduce below 10 degrees')
+    } else {
+        this.temperature --
+    }  
+}
+
+Thermostat.prototype._powerSaveOff = function() {
+    this.powerSaver = false
+    this.maxtemp = 32
+}
+
+Thermostat.prototype._powerSaveOn = function() {
+    this.powerSaver = true
+    this.maxtemp = 25
+}
+
+Thermostat.prototype._reset = function() {
+    this.temperature = this.DEFAULT_TEMP
+}
+
+Thermostat.prototype._usage = function() {
+    if (this.temperature < 18) {
+        return 'low-usage'
+    } else if (this.temperature < 25) {
+        return 'medium-usage'
+    }    if (this.temperature >= 25) {
+        return 'high-usage'
+    }
 }
